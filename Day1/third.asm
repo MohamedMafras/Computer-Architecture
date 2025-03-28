87e6aeb94747858d122fb496aa681276146da30e
MohamedMafras <120329346+MohamedMafras@users.noreply.github.com>
2025-03-28 17:14:35 +0530

    Update third.asm

--- Day1/third.asm
+++ Day1/third.asm
@@ -1,17 +1,20 @@
 .model small
 .stack 100h
+
 .data
-  msg db "Hello",13,"Hi$"
+
+n db 5
+
 .code
 start:
-  mov ax,@data
-  mov ds,ax
-  
-  mov dx, offset msg
-  mov ah, 09h
-  int 21h
-  
-  mov ax,4c00h
-  int 21h
-  
+
+mov ax, @data
+mov ds, ax
+mov dl, n
+add dl, 48
+mov ah, 02h
+int 21h
+
+mov ax, 4c00h
+int 21h
 end start
